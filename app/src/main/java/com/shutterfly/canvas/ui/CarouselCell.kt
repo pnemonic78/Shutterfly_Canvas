8879f8edb25b23cb5d9f1f647fffda6180192193
Moshe W <pnemonic@gmail.com>
2025-08-20 12:25:46 +0300

    images lib

--- app/src/main/java/com/shutterfly/canvas/ui/CarouselCell.kt
+++ app/src/main/java/com/shutterfly/canvas/ui/CarouselCell.kt
@@ -1,8 +1,9 @@
 package com.shutterfly.canvas.ui
 
+import android.content.Context
 import android.net.Uri
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
@@ -13,22 +14,36 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import coil3.request.ImageRequest
+import com.shutterfly.canvas.R
 import com.shutterfly.canvas.ui.theme.ShutterflyCanvasTheme
 
 private val checkboxSize = 48.dp
 
 @Composable
 fun CarouselCell(modifier: Modifier = Modifier, thumbnail: Uri) {
+    val context: Context = LocalContext.current
     var selected by remember { mutableStateOf(false) }
 
     Box(
         modifier = modifier
             .clip(MaterialTheme.shapes.medium)
-            .background(color = Color.Yellow)
     ) {
+        AsyncImage(
+            modifier = Modifier.fillMaxSize(),
+            model = ImageRequest.Builder(context)
+                .data(thumbnail)
+                .build(),
+            placeholder = painterResource(R.drawable.image),
+            contentDescription = thumbnail.toString(),
+            contentScale = ContentScale.Crop
+        )
         Checkbox(
             modifier = Modifier.size(checkboxSize),
             checked = selected,
@@ -43,6 +58,6 @@ fun CarouselCell(modifier: Modifier = Modifier, thumbnail: Uri) {
 @Composable
 private fun CanvasScreenPreview() {
     ShutterflyCanvasTheme {
-        CarouselCell(thumbnail = Uri.EMPTY)
+        CarouselCell(modifier = Modifier.size(200.dp), thumbnail = Uri.EMPTY)
     }
 }
--- app/src/main/java/com/shutterfly/canvas/view/CanvasViewModel.kt
+++ app/src/main/java/com/shutterfly/canvas/view/CanvasViewModel.kt
@@ -1,20 +1,38 @@
 package com.shutterfly.canvas.view
 
 import android.net.Uri
+import androidx.core.net.toUri
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.shutterfly.canvas.model.CanvasImage
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class CanvasViewModel : ViewModel() {
-    val gallery: StateFlow<List<Uri>> = MutableStateFlow(emptyList())
+    private val _gallery = MutableStateFlow(emptyList<Uri>())
+    val gallery: StateFlow<List<Uri>> = _gallery
     val canvasImages: StateFlow<List<CanvasImage>> = MutableStateFlow(emptyList())
+
+    private val galleryImageNames = listOf<String>(
+        "building-7758389_640.jpg",
+        "drinking-cups-7660117_640.jpg",
+        "europe-9753774_640.jpg",
+        "milky-way-9767930_640.jpg",
+        "tools-9754352_640.jpg"
+    )
 
     init {
         loadGallery()
     }
 
     private fun loadGallery() {
-        //TODO("Not yet implemented")
+        viewModelScope.launch(Dispatchers.IO) {
+            val uris = galleryImageNames.map { name ->
+                "file:///android_asset/$name".toUri()
+            }
+            _gallery.emit(uris)
+        }
     }
 }
